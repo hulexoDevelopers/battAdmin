b06e2c11048ba3d82aa2e929cfbe1b37c32e18a4
abanchaudry <abanchaudry@gmail.com>
2022-06-11 00:45:57 +0500

    remove apex-chart

--- src/app/modules/adminModule/admin.module.ts
+++ src/app/modules/adminModule/admin.module.ts
@@ -3,19 +3,17 @@ import { CommonModule } from '@angular/common';
 import { routedComponents, adminRoutingModule } from './admin-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core';
-// import { SelectDropDownModule } from 'ngx-select-dropdown';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { SelectDropDownModule } from 'ngx-select-dropdown';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
-import { NgApexchartsModule } from 'ng-apexcharts';
+
 @NgModule({
   declarations: [routedComponents],
   imports: [
     CommonModule,
     adminRoutingModule,
     Ng2SearchPipeModule,
-    NgApexchartsModule,
     NgxDaterangepickerMd,
     NgxPaginationModule,
     SelectDropDownModule,
@@ -23,6 +21,5 @@ import { NgApexchartsModule } from 'ng-apexcharts';
     CoreModule
   ],
   providers: [],
-  // entryComponents: [allMealsCalendarComponent]
 })
 export class adminModule { }
